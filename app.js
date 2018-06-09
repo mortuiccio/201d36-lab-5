cb6decb5750d6a7f96488022167503cebc78e8d5
mortuiccio <keithmortuiccio@gmail.com>
2018-06-08 22:38:52 -0700

    third pass

--- app.js
+++ app.js
@@ -54,11 +54,19 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+ var add = sum (a, b)
+ var addAgain = sum (add[0], c) 
+ var times = multiply (a, b)
+ var timeTime = multiply (times[0], c)
+ var mess1 = a + ' and ' + b + ' and ' + c + ' sum to ' + addAgain[0] + '.'
+ var mess2 = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + timeTime[0] + '.'
+ console.log(addAgain[0], timeTime[0], mess1, mess2)
+ return [addAgain[0], timeTime[0], mess1, mess2 ]
+ 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
